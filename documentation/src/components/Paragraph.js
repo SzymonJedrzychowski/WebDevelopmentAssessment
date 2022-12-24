10da6deb8b05b5c219a6a99b895b5b093e8e059c
SzymonJedrzychowski <czarmistrz@outlook.com>
2022-12-24 17:24:41 +0100

    improve styling for endpoint documentation

--- documentation/src/components/Paragraph.js
+++ documentation/src/components/Paragraph.js
@@ -13,31 +13,35 @@ function Paragraph(props) {
 
     const processValue = (value) => {
         if (Array.isArray(value)) {
-            const temp = value.map((value, key) => <Paragraph data={value} key={key}/>)
+            const temp = value.map((value, key) => <Paragraph data={value} key={key} />)
 
             return <ListGroup>
                 {temp}
             </ListGroup>
         }
         return (
-            <>
-                <ListGroup.Item action onClick={showDetails}>
-                    <h2>{value.title}</h2>
-                </ListGroup.Item>
+            <div className={value.class}>
+                <div className="clickable">
+                    {!visible &&
+                        <svg onClick={showDetails} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 3h18v18H3zM12 8v8m-4-4h8" /></svg>}
+                    {visible && 
+                        <svg onClick={showDetails} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 3h18v18H3zM8 12h8" /></svg>}
+                    <h2 onClick={showDetails}>{value.title}</h2>
+                </div>
                 {visible &&
-                    <>
+                    <div className="dataContent">
                         {value.type && <p>Type: {value.type}</p>}
                         {value.text && <p>{value.text}</p>}
                         {Array.isArray(value.data) && <Paragraph data={value.data} />}
-                    </>
+                    </div>
                 }
-            </>);
+            </div>);
     }
 
     return (
-        <ListGroup>
+        <>
             {processValue(props.data)}
-        </ListGroup>
+        </>
     );
 }
 
